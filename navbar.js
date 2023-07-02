a825ce2290cadcbc1cb77ecd279d5ac9f271c4f1
Kwinten123 <quinten.hoogkamer@gmail.com>
2023-07-02 13:56:46 +0200

    added linkedin relocation

--- navbar.js
+++ navbar.js
@@ -2,15 +2,22 @@ class Myheader extends HTMLElement {
     connectedCallback() {
         this.innerHTML =
             '<header id="header">\n' +
-                '<nav>\n' +
+                '<nav class="nav-bar">\n' +
                     '<ul id="navlist">\n' +
                         '<li class="list-item"><img class="logo" src="img/logo.jpeg" alt="logo"></li>\n' +
                         '<li class="list-item"><a class="anchor" href="index.html">Thuis</a></li>\n' +
                         '<li class="list-item"><a class="anchor" href="hardlopen.html">Hardlopen</a></li>\n' +
                         '<li class="list-item"><a class="anchor" href="wandelen.html">Wandelen</a></li>\n' +
                     '</ul>\n' +
+                    '<div class="linkedin-img" onclick="relocateLinkedin()"><img src="img/LinkedIn_logo_.png" alt="linkedIn"/></div>\n' +
                 '</nav>\n' +
             '</header>\n'
     }
 }
 customElements.define('my-header', Myheader)
+
+
+function relocateLinkedin(){
+    const linkedInLink = "https://linkedin.com/in/femke-kaptein-6678a06"
+    window.location = linkedInLink
+}
